6e3ca14f8161268e3c64b70c9e0a9181ff912562
eduardobispof <eduardobispof@gmail.com>
2019-05-08 15:50:33 -0300

    adicionando metodos para receber linhas dos arquivos

--- FileManager.java
+++ FileManager.java
@@ -1,25 +1,67 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class FileManager {
     FileReader arquivo;
     BufferedReader arquivoBuffer;
-
-    public FileManager() {
-        setArquivo(arquivo);
-    }
+    String linha;
 
     /**
      * @return the arquivo
      */
     public FileReader getArquivo() {
-        return arquivo;
+        return this.arquivo;
     }
 
     /**
      * @param arquivo the arquivo to set
      */
-    public void setArquivo(FileReader arquivo) {
-        this.arquivo = arquivo = new FileReader("estoque.csv");
+    public void setArquivo() throws IOException {
+        this.arquivo = new FileReader("estoque.csv");
     }
+
+    /**
+     * @return the arquivoBuffer
+     */
+    public BufferedReader getArquivoBuffer() {
+        return this.arquivoBuffer;
+    }
+
+    /**
+     * @param arquivoBuffer the arquivoBuffer to set
+     */
+    public void setArquivoBuffer(FileReader arquivoBuffer) {
+        this.arquivoBuffer = new BufferedReader(arquivoBuffer);
+    }
+
+    /**
+     * @return the linha
+     */
+    public String getLinha() {
+        return this.linha;
+    }
+
+    /**
+     * @param linha the linha to set
+     */
+    public void setLinha(BufferedReader arquivoBuffer) throws IOException {
+        this.linha = getArquivoBuffer().readLine();
+    }
+
+    public static void main(String[] args) {
+        try {
+            FileManager teste = new FileManager();
+            teste.setArquivo();
+            teste.setArquivoBuffer(teste.getArquivo());
+            teste.setLinha(teste.getArquivoBuffer());
+
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("Fim do programa");
+        }
+        System.out.println("OK");
+    }
+
 }
